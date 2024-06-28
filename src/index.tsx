2b39b17971280666e96e661b6deac3b97226342a
Yinon <yinonmegi2212@gmail.com>
2024-06-28 18:12:07 +0300

    Ready to deploy

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
-if(proccess.env.NODE_ENV === 'production') disableReactDevTools();
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/socket.js
+++ src/socket.js
@@ -2,4 +2,5 @@ import { io } from 'socket.io-client';
 import {URL} from './Globals/consts';
 import socketIO from 'socket.io-client';
 
+
 export const socket = socketIO.connect(URL);
